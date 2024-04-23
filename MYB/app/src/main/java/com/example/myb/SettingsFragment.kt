631debe0f4c179e26e0274cd17d5d7f0f944eb9f
OleksandrVlasiuk <alexvlasyk31@gmail.com>
2024-04-23 17:08:16 +0300

    header

--- MYB/app/src/main/java/com/example/myb/SettingsFragment.kt
+++ MYB/app/src/main/java/com/example/myb/SettingsFragment.kt
@@ -1,10 +1,11 @@
 package com.example.myb
+
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        // Використайте XML-файл з ресурсів для завантаження налаштувань
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        // Використайте змінений XML-файл з ресурсів для завантаження налаштувань
+        setPreferencesFromResource(R.xml.settings, rootKey)
     }
 }
